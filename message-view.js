39a2de51facac41b901f6f0ecdb84de452149a80
Yousef-Hatem <yousef26hatem@gmail.com>
2022-07-06 23:40:26 +0200

    2.3.8

--- message-view.js
+++ message-view.js
@@ -4,28 +4,28 @@ messageView = {
 
     radio: { error: '', correct: '' },
 
-    email: { error: 'sorry, this email is incorrect', correct: '' },
+    email: { error: 'Sorry, this email is incorrect', correct: '' },
 
-    required: { error: 'this field is required', correct: '' },
+    required: { error: 'This field is required', correct: '' },
 
-    number: { error: 'the figure is incorrect', correct: '' },
+    number: { error: 'The figure is incorrect', correct: '' },
 
-    integer: { error: 'the figure is incorrect', correct: '' },
+    integer: { error: 'The figure is incorrect', correct: '' },
 
-    file: { error: 'this file format is incorrect', correct: '' },
+    file: { error: 'This file format is incorrect', correct: '' },
 
-    url: { error: 'sorry, but this link is incorrect', correct: '' },
+    url: { error: 'Sorry, but this link is incorrect', correct: '' },
 
-    phone: { error: 'sorry, but this phone number is incorrect', correct: '' },
+    phone: { error: 'Sorry, but this phone number is incorrect', correct: '' },
 
-    resetPassword: { error: 'sorry, but the password does not match', correct: '' },
+    resetPassword: { error: 'Sorry, but the password does not match', correct: '' },
 
     password: {
-        simple: { error: 'sorry, but the password must be at least 8', correct: '' },
+        simple: { error: 'Sorry, but the password must be at least 8', correct: '' },
 
-        complex: { error: 'sorry, but the password must be at least 8 and contain letters and numbers', correct: '' },
+        complex: { error: 'Sorry, but the password must be at least 8 and contain letters and numbers', correct: '' },
 
-        difficult: { error: 'sorry, but the password must be at least 8 and contain uppercase and lowercase letters, numbers and symbols', correct: '' }
+        difficult: { error: 'Sorry, but the password must be at least 8 and contain uppercase and lowercase letters, numbers and symbols', correct: '' }
     }
 }
 
